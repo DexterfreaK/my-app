f0cf6c9c7c7d47d581bc5ff5a0ce976c32390590
JManan <manan.jain.0912@gmail.com>
2023-04-06 03:32:23 +0530

    tabs created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Box, Tab } from '@mui/material';
+import { TabList, TabContext, TabPanel } from '@mui/lab';
 
 function App() {
+  const [value, setValue] = useState("1")
+  const handleChange = (event: React.SyntheticEvent, newvalue : string) =>{
+    setValue(newvalue)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+  <TabContext value={value}>
+<Box sx={{ borderColor: 'divider' }}>
+  <TabList color="primary" onChange={handleChange} textColor='secondary' centered>
+<Tab  label="All" value='1'/>
+<Tab  label="Doing" value='2' />
+<Tab  label="Done" value='3'/>
+  </TabList>
+</Box>
+<TabPanel value="1">Panel One</TabPanel>
+<TabPanel value="2">Panel two</TabPanel>
+<TabPanel value="3">Panel three</TabPanel>
+  </TabContext>
+</>
   );
 }
 
