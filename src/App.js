1cf1dbbd0ce3bc420aafd3fe37faa28caafb2fd3
JManan <manan.jain.0912@gmail.com>
2023-04-06 21:45:45 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import { Box, Tab, Card, CardContent, Typography } from '@mui/material';
+import { Box, Tab, Card, CardContent, CardActions, CardMedia, Typography, Button } from '@mui/material';
 import { TabList, TabContext, TabPanel } from '@mui/lab';
 
 function App() {
@@ -8,17 +8,22 @@ function App() {
   const handleChange = (event: React.SyntheticEvent, newvalue : string) =>{
     setValue(newvalue)
   }
-  const [doneCard, setdoneCard] = useState([{
+  var [doneCard, setdoneCard] = useState([{
     'id':1, 'title': "Card1 title", "description" : "this is card1 decscription"},
     {'id':2, 'title': "Card2 title", "description" : "this is card2 decscription"},
     {'id':3, 'title': "Card3 title", "description" : "this is card3 decscription"}
 ])
 
-  const [doingCard, setdoingCard] = useState([
+  var [doingCard, setdoingCard] = useState([
     {'id':4, 'title': "Card4 title", "description" : "this is card4 decscription"},
     {'id':5, 'title': "Card5 title", "description" : "this is card5 decscription"},
     {'id':6, 'title': "Card6 title", "description" : "this is card6 decscription"}
 ])
+
+  const handleMovetoDone = (card) => {
+    setdoingCard(doingCard.filter(c => c.id !== card.id))
+    setdoneCard(...doneCard, card)
+  }
   return (
 <>
   <TabContext value={value}>
@@ -34,6 +39,8 @@ function App() {
 <div>
 <Box width="300px">
   <Card key={card.id}>
+    {/* <CardMedia component='img' height='140' image="https://source.unsplash.com/random">
+    </CardMedia> */}
     <CardContent>
       <Typography gutterbottom variant='h5'>{card.title}</Typography>
       <Typography gutterbottom variant='body'>{card.description}</Typography>
@@ -42,7 +49,7 @@ function App() {
 </Box>
 </div>
 )}
-  {doneCard.map((card) => 
+  {doneCard.map((card) =>
 <div>
 <Box width="300px">
   <Card key={card.id}>
@@ -50,14 +57,47 @@ function App() {
       <Typography gutterbottom variant='h5'>{card.title}</Typography>
       <Typography gutterbottom variant='body'>{card.description}</Typography>
     </CardContent>
+
   </Card>
 </Box>
 </div>
 )}
 </>
 </TabPanel>
-<TabPanel value="2"></TabPanel>
-<TabPanel value="3">Panel three</TabPanel>
+<TabPanel value="2">
+{doingCard.map((card) => 
+<div>
+<Box width="300px">
+  <Card key={card.id}>
+    <CardContent>
+      <Typography gutterbottom variant='h5'>{card.title}</Typography>
+      <Typography gutterbottom variant='body'>{card.description}</Typography>
+    </CardContent>
+    <CardActions>
+    <Button size='small' onClick={() => {handleMovetoDone(card)}}> Move to Done!</Button>
+    </CardActions>
+  </Card>
+</Box>
+</div>
+)}
+</TabPanel>
+<TabPanel value="3"><div>
+{doneCard.map((card) => 
+<div>
+<Box width="300px">
+  <Card key={card.id}>
+    <CardContent>
+      <Typography gutterbottom variant='h5'>{card.title}</Typography>
+      <Typography gutterbottom variant='body'>{card.description}</Typography>
+    </CardContent>
+    <CardActions>
+    <Button size='small'> Move to Doing!</Button>
+    </CardActions>
+  </Card>
+</Box>
+</div>
+)}</div>
+</TabPanel>
   </TabContext>
 </>
   );
